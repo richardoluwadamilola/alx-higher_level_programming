5ec34af4fe4eaf6ffa797d100205bf228ba1ed5d
RIchard Oluwadamilola <oluwatobyrichard@gmail.com>
2022-09-20 13:02:47 +0100

    py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    if (i % 2 == 0):
-        print("{}".format(chr(i)), end="")
-    else:
-        print("{}".format(chr(i - 32)), end="")
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{:c}".format((c - (ord('a') - ord('A'))) if c % 2 else c), end='')
