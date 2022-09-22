fac89021950a7e0f1683e0cdcd89efdbfedb7104
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-09-22 04:58:02 -0700

    d

--- 0x02-python-import_modules/2-args.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    i = len(argv)
-    print("{:d} {:s}{:s}".format(i - 1, "argument" if i <= 2 else "arguments",
-                                 "." if i == 1 else ":"))
-    for i, s in enumerate(argv):
-        if i > 0:
-            print("{:d}: {:s}".format(i, s))
