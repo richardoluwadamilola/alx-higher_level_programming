c9b5bba2d8a205a098cda07217f5883707548ef0
RIchard Oluwadamilola <oluwatobyrichard@gmail.com>
2022-09-22 13:19:48 +0100

    fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        quit(1)
-    a = int(argv[1])
-    b = int(argv[3])
-    ops = ["+", "-", "*", "/"]
     from calculator_1 import add, sub, mul, div
-    funcs = [add, sub, mul, div]
-    for i, s in enumerate(ops):
-        if argv[2] == s:
-            print("{} {} {} = {}".format(a, s, b, funcs[i](a, b)))
-            break
+    from sys import argv
+    if len(argv) - 1 != 3:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        exit(1)
+    if argv[2] == '+':
+        print("{} + {} = {}"
+              .format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
+    elif argv[2] == '-':
+        print("{} - {} = {}"
+              .format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
+    elif argv[2] == '*':
+        print("{} * {} = {}"
+              .format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
+    elif argv[2] == '/':
+        print("{} / {} = {}"
+              .format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        quit(1)
+        exit(1)
