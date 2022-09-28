8090d3da173399229a8634ce1f4416698d850c3c
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-09-28 04:36:25 -0700

    fixed

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    for i in range(len(new_list)):
-        if (new_list[i] == search):
-            new_list[i] == replace
-
-    return (new_list)
+    return (list(map(lambda x: replace if x is search else x, my_list)))
