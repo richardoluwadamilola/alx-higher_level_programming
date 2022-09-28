c549708b958db282d734689bf5e7cfbd366a1ba7
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-09-28 04:49:17 -0700

    doe

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for key in sorted(my_dict.keys()):
-        print("{}: {}".format(keys, my_dict[keys]))
+    for key in sorted(a_dictionary.keys()):
+        print('{}: {}'.format(keys, a_dictionary[keys]))
