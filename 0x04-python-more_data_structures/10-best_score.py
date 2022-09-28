9a30ddb5281e30ccecbebecb4b817768305c7317
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-09-28 03:51:53 -0700

    return the key with maximum integer value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if my_dict and len(my_dict):
+        max = list(my_dict.keys())[0]
+        for key in my_dict:
+            if my_dict[key] > my dict[max]:
+                max = key
+        return max
+    return None
