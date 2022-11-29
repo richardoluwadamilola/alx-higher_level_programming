0ac226107857f5c5d2a3e150f379c94c73788283
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-11-29 05:27:27 -0800

    conversion of the database to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- Conversion of htbn_0c_0 database to UTF8 and query for convertion
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET  utf8mb4 COLLATE utf8mb4_unicode_ci;
