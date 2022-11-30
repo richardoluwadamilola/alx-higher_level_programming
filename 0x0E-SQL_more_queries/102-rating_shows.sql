c83ea4c6e4909be7d3b84fbea13808f17031c6ce
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-11-30 03:40:34 -0800

    list shows by their ratings in descending order

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- Lists all shows in the dump database by hteir ratings
+SELECT `title`, SUM(`rate`) AS `rating`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_ratings` AS r
+       ON t.`id` = r.`show_id`
+ GROUP BY `title`
+ ORDER BY `rating` DESC;
