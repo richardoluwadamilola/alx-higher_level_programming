493453f425cccc41f12681785fb14235689fc0f1
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-11-30 03:16:50 -0800

    fix

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,10 @@
 -- Lists of all genres of the show title Dexter in the import data dump, order in asc
-SELECT tv_genres.name
-FROM tv_genres
-INNER JOIN tv_shows_genres
-ON tv_genres.id = tv_shows_genres.genre_id
+SELECT g.`name`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON g.`id` = s.`genre_id`
 
-INNER JOIN tv_shows
-ON tv_shows.id=tv_shows_genres.show_id
-WHERE tv_shows.title = "Dexter"
-ORDER BY tv_genres.name;
+       INNER JOIN `tv_shows` AS t
+       ON t.`id` = s.`show_id`
+       WHERE t.`title` = "Dexter"
+ ORDER BY g.`name`;
