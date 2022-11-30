1fbe8e803d00ce7ae58a7391bbc2cc324af09230
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-11-30 03:10:25 -0800

    fix

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,6 +3,7 @@ SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_shows_genres
 ON tv_genres.id = tv_shows_genres.genres_id
+
 INNER JOIN tv_shows
 ON tv_shows.id=tv_shows_genres.show_id
 WHERE tv_shows.title = "Dexter"
