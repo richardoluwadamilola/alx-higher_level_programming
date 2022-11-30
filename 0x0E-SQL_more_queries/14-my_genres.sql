fd414700bd26c677148434cf39fff257072eaefd
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-11-30 02:56:34 -0800

    lists of genres of show titled Dexter

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- Lists of all genres of the show title Dexter in the import data dump, order in asc
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_shows_genres
+ON tv_genres.name.id = tv_shows_genres.genres_id
+INNER JOIN tv_shows
+ON tv_shows.id=tv_shows_genres.show_id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name;
